54a0172fdf95c77f9f30161fd64b76dde29812d0
sn482 <nie24017@byui.edu>
2025-01-14 23:43:03 +0000

    changed it to the loops in the assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random random = new Random();
+        int magicNumber = random.Next(1,101);
+        int guess = 0;
+
+        Console.WriteLine("Welcome to the Magic Number game");
+
+        while (guess != magicNumber)
+        {
+            Console.Write("what is your guess: ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You got it Good Job");
+            }
+        }
     }
+
 }
