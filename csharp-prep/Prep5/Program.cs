981c5c8a53d59c9e88f4cb89664c2f279adee089
sn482 <nie24017@byui.edu>
2025-01-16 19:53:20 +0000

    changed it to the functions part of c prep

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int favoriteNumber = PromptUserNumber();
+
+        int squaredNumber = SquareNumber(favoriteNumber);
+
+        // Call DisplayResult to display the user's name and squared number
+        DisplayResult(userName, squaredNumber);
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string userResponse = Console.ReadLine();
+        return int.Parse(userResponse);
+    }
+
+    static int SquareNumber(int num)
+    {
+        return num * num;
+    }
+
+    static void DisplayResult(string name, int squaredNum)
+    {
+        Console.WriteLine($"{name}, the square of your number is {squaredNum}");
     }
 }
